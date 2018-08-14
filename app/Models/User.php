db878543f3048ee02e98fa5f3f5a0214e832c0ae
Benjamim Thiago <bebenjamimthiago@gmail.com>
2018-08-14 14:51:00 -0300

    abas do menu edit perfil usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements TableInterface {
         'name', 'email', 'password', 'enrolment'
     ];
 
+    
     public static function createFully($data)
     {
         $password = str_random(6);
@@ -38,30 +39,30 @@ class User extends Authenticatable implements TableInterface {
         }
         return compact('user', 'password');
     }
-
+    
     public static function assignEnrolment(User $user, $type) {
         $types = [
             self::ROLE_ADMIN => 100000,
             self::ROLE_TEACHER => 400000,
             self::ROLE_STUDENT => 700000,
         ];
-
+        
         $user->enrolment = $types[$type] + $user->id;
         return $user->enrolment;
     }
-
+    
     public static function assignRole(User $user, $type) {
         $types = [
             self::ROLE_ADMIN => Admin::class,
             self::ROLE_TEACHER => Teacher::class,
             self::ROLE_STUDENT => Student::class,
         ];
-
+        
         $model = $types[$type];
         $model = $model::create([]);
         $user->userable()->associate($model);
     }
-
+    
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -70,7 +71,12 @@ class User extends Authenticatable implements TableInterface {
     protected $hidden = [
         'password', 'remember_token',
     ];
-
+    
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class)->withDefault();
+    }
+    
     public function userable()
     {
         return $this->morphTo();

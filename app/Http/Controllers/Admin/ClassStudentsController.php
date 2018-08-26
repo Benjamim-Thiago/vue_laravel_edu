72a38602748b231e71e07afe604c0ea2b87d9b90
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2018-08-26 18:08:26 -0300

    criação do service, store e componente de listagem de alunos por turma

--- app/Http/Controllers/Admin/ClassStudentsController.php
+++ app/Http/Controllers/Admin/ClassStudentsController.php
@@ -9,10 +9,14 @@ use BEN\Http\Controllers\Controller;
 class ClassStudentsController extends Controller
 {
    
-    public function index(ClassInformation $class_information)
+    public function index(Request $request, ClassInformation $class_information)
     {
-        return view('admin.class_informations.class_student', compact('class_information'));
+        if(!$request->ajax()){
+            return view('admin.class_informations.class_student', compact('class_information'));
+        }
+        return $class_information->students()->get();
     }
+
 
 
     
--- resources/assets/admin/js/admin.js
+++ resources/assets/admin/js/admin.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('class-student', require('./components/class-information/ClassStudent.vue'));
 
 const app = new Vue({
     el: '#app'
